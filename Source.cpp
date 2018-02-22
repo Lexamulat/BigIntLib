76582ae91dbb002db237864d5f39edf751f843f8
lexamulat <lexamulat@mail.ru>
2018-02-22 21:00:11 +0300

    summ work

--- Source.cpp
+++ Source.cpp
@@ -8,139 +8,130 @@
 using namespace std;
 
 
-void StrBin(string &input, string &support, vector<int>& array1, int &num, int&curr, bool &end, bool&f) {
+bool StrToVect(string &input, vector<int>& array) {
 
-	if (end == 1) {
-		return;
+
+	bool znk = 0;
+	if (input[0] == '-') {
+		znk = 1;
+		input.erase(0, 1);
+
 	}
-	if (input[0] == '1') {
-		f = 1;
+	if (input[0] == '+') {
+		znk = 0;
+		input.erase(0, 1);
+
 	}
+
 	for (int i = 0; i < input.length(); i++) {
-
-		num = input[i] - 48;
-
-		if ((curr == 0) && (f == 0)) {
-			curr = num % 2;
-
-			if (i == 0) {
-				support = ((num / 2) + 48);
-			}
-			else {
-				support += ((num / 2) + 48);
-			}
-		}
-		else if (f == 0) {
-			curr = (curr * 10) + num;
-			support += ((curr / 2) + 48);
-			curr = curr % 2;
-		}
-		else if (f == 1) {
-			curr = 1;
-			f = 0;
-		}
+		array.insert(array.end(), input[i] - 48);
 
 	}
-
-
-
-	array1.insert(array1.end(), curr);
-	if ((support.length() < 2) && ((support[0] - 48) < 2)) {
-		array1.insert(array1.end(), (support[0] - 48));
-		end = 1;
-		return;
-	}
-	else {
-		input = support;
-		support.clear();
-
-		curr = 0;
-		StrBin(input, support, array1, num, curr, end, f);
-	}
+	return znk;
 }
 
-void summ(vector<int>& array1, vector<int>& array2,bool&z1,bool&z2) {
-	if ((array1.size())!= (array2.size())) {
-		if ((array1.size()) > (array2.size())) {
-			for (int i = 0; i < ((array1.size()) - (array2.size())); i++) {
-				array2.insert(array2.begin(), 0);
-			}
+
+void Summ(vector<int>& array1, vector<int>& array2, vector<int>& result) {
+	int curr;
+	int buff=0;
+
+	int raz = (array1.size() - array2.size());
+	if (array1.size() > array2.size()) {
+	
+		for (int i = 0; i <raz;i++){
+			array2.insert(array2.begin(), 0);
+		
+		}
+	
+	}
+	else if (array1.size() < array2.size()) {
+	
+		for (int i = 0; i < (raz*(-1)); i++) {
+			array1.insert(array1.begin(), 0);
+
+		}
+	}
+
+	for (int i = array1.size()-1; i > -1;i--) {
+		curr = (array1[i] + array2[i])+buff;
+		if (curr < 10) {
+			result.insert(result.begin(), curr);
+			buff = 0;
+
 		}
 		else {
-			for (int i = 0; i < ((array2.size()) - (array1.size())); i++) {
-				array1.insert(array1.begin(), 0);
-			}
+			result.insert(result.begin(), (curr%10));
+			buff = curr/10;
 		}
-
-	}
-
-	cout <<endl<< "ar1" << endl;
-	for (int i = 0; i < array1.size(); i++) {
-		cout << array1[i];
-	}
-	cout <<endl<< "ar2" << endl;
-
-	for (int i = 0; i < array2.size(); i++) {
-		cout << array2[i];
-	}
-
-	while ((array2.size())<7) {
-		array2.insert(array2.begin(), 0);
 	
 	}
 
-	while ((array1.size())<7) {
-		array1.insert(array1.begin(), 0);
-
+	if (buff != 0) {
+		result.insert(result.begin(), buff);
 	}
 
-	cout << endl << "ar1" << endl;
+	cout << endl << "--------------result----------------------------" << endl;
+
+
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i];
+	}
+
+
+/*
+	cout <<endl<< "------------------------------------------" << endl;
+
+
 	for (int i = 0; i < array1.size(); i++) {
 		cout << array1[i];
 	}
-	cout << endl << "ar2" << endl;
-
+	cout <<endl<< "------------------------------------------" << endl;
 	for (int i = 0; i < array2.size(); i++) {
 		cout << array2[i];
 	}
-
+	cout <<endl<< "------------------------------------------" << endl;*/
 }
 
-// fd
+
+
+
+
+
 
 
 
 int main() {
 
-	//
+	
 	vector<int> array1;
 	vector<int> array2;
+	vector<int> result;
 	string input1,input2;
-	string support;
-	bool end = 0;
+	bool z1 = 0, z2 = 0;
 	cout << "enter 1" << endl;
 	cin >> input1;
 	cout << "enter 2" << endl;
 	cin >> input2;
-	int curr = 0;
-	int num;
-	bool f = 0;
-	bool z1=0;
-	if (input1[0] == '-') {
-		z1 = 1;
-	}
-	bool z2= 0;
-	if (input2[0] == '-') {
-		z1 = 1;
-	}
 
-	input1.erase(0, 1);
-	input2.erase(0, 1);
-
-	cout <<"inp1:" <<input1 << endl;
 	
-	StrBin(input1, support,array1,num,curr,end,f);
+	z1=StrToVect(input1, array1);
+	z2 = StrToVect(input2, array2);
+
+	cout << endl << z1 << endl;
+
 	for (int i = 0; i < array1.size(); i++) {
+		cout << array1[i];
+	}
+	cout << endl << z2 << endl;
+
+	for (int i = 0; i < array2.size(); i++) {
+		cout << array2[i];
+	}
+
+
+	Summ(array1,array2,result);
+
+	/*for (int i = 0; i < array1.size(); i++) {
 		cout << array1[i];
 	}
 	cout << endl << "-----" << endl;
@@ -171,7 +162,7 @@ int main() {
 		cout << array2[i];
 	}
 
-	summ(array1,array2,z1,z2);
+	summ(array1,array2,z1,z2);*/
 
 
 	system("pause");
