c5e9b0dfc4fd18294e2094f9e61486a457b6e44c
lexamulat <lexamulat@mail.ru>
2018-02-27 22:05:42 +0300

    1

--- big.cpp
+++ big.cpp
@@ -232,9 +232,7 @@ big big::diff(big &num1, big &num2, bool &PlaseOfCall) {
 			result.insert(result.begin(), 0);
 			big out(result, *num2.znak);
 		}
-		while (result[0] == 0) {
-			result.erase(result.begin() + 0);
-		}
+		
 
 		/*cout << endl << "--------------result----------------------------" << endl;
 
@@ -250,83 +248,5 @@ big big::diff(big &num1, big &num2, bool &PlaseOfCall) {
 }
   
 
-//void Diff(vector<int>& array1, vector<int>& array2, vector<int>& result) {
-//	result.clear();
-//	int buff=0;
-//	int curr;
-//	//////////////////////////////////////////////////////
-//	int raz = (array1.size() - array2.size());
-//	if (array1.size() > array2.size()) {
-//
-//		for (int i = 0; i <raz; i++) {
-//			array2.insert(array2.begin(), 0);
-//
-//		}
-//
-//	}
-//	else if (array1.size() < array2.size()) {
-//
-//		for (int i = 0; i < (raz*(-1)); i++) {
-//			array1.insert(array1.begin(), 0);
-//
-//		}
-//	}
-//	////////////////////////////////////////////////////////      normal sizes
-//	int big=0;
-//	for (int i = 0; i < array1.size(); i++) {
-//		if (array1[i] > array2[i]) {
-//			big = 1;
-//			break;
-//		}
-//		else if (array1[i] < array2[i]) {
-//			big = 2;
-//			break;
-//		}
-//	}
-//	///////////////////////////////////////////////////////   find   the biggest num 
-//
-//	if (big == 1) {
-//		for (int i = array1.size() - 1; i > -1; i--) {
-//			curr = (array1[i]-buff) - array2[i];
-//			if (curr<0) {
-//				curr = ((10 + array1[i]) - array2[i]);
-//				buff = 1;
-//			}
-//			else if (curr>=0) {
-//				buff = 0;
-//				
-//			}
-//			result.insert(result.begin(), curr);
-//		}
-//		cout << endl << "+" << endl;
-//	}
-//
-//	if (big == 2) {
-//		for (int i = array1.size() - 1; i > -1; i--) {
-//			curr = (array2[i] - buff) - array1[i];
-//			if (curr<0) {
-//				curr = ((10 + array2[i]) - array1[i]);
-//				buff = 1;
-//			}
-//			else if (curr >= 0) {
-//				buff = 0;
-//
-//			}
-//			result.insert(result.begin(), curr);
-//		}
-//	
-//	
-//		cout << endl << "-" << endl;
-//	
-//	}
-//
-//	if (big == 0) {
-//	
-//		result.insert(result.begin(), 0);
-//	}
-//
-//	/*cout << endl << "--------------result----------------------------" << endl;
-//
-//	for (int i = 0; i < result.size(); i++) {
-//		cout << result[i];
-//	}*/
+
+
